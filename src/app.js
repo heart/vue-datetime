36e04111f7c212bad2c3ac95d2bd0bdd7f34b153
Heart Narongrit <narongrit@3dsinteractive.com>
2023-04-07 09:28:22 +0700

    Updates

---
+++ src/app.js
@@ -0,0 +1,31 @@
+import Vue from 'vue'
+import { DateTime as LuxonDateTime } from 'luxon'
+import Datetime from '../../dist/vue-datetime'
+import '../../dist/vue-datetime.css'
+import './app.css'
+
+Vue.use(Datetime)
+
+new Vue({
+  el: '#app',
+  methods: {
+    dateDisplayFunction (date) {
+      console.log('custom', typeof date)
+      // let d = new Date(date)
+      return `${date.getDate()} - ${date.getMonth()} - ${date.getFullYear()}`
+    }
+  },
+  data () {
+    return {
+      time: '19:06',
+      date: '2018-05-12T00:00:00.000Z',
+      datetime: '2018-05-12T17:19:06.151Z',
+      datetime12: '2018-05-12T17:19:06.151Z',
+      datetime13: '2018-05-12T17:19:06.151Z',
+      datetimeEmpty: '',
+      minDatetime: LuxonDateTime.local().minus({ month: 1, days: 3 }).toISO(),
+      maxDatetime: LuxonDateTime.local().plus({ days: 3 }).toISO(),
+      datetimeTheming: LuxonDateTime.local().toISO()
+    }
+  }
+})
